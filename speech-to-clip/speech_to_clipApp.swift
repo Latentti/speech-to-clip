b747d228ffccadc5f8626a6de0fe3121fa8b21eb
AriHietamaki <ari@hietamaki.org>
2025-11-12 18:12:50 +0200

    Initial Commit

---
+++ speech-to-clip/speech_to_clipApp.swift
@@ -0,0 +1,17 @@
+//
+//  speech_to_clipApp.swift
+//  speech-to-clip
+//
+//  Created by Ari Hietamäki on 12.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct speech_to_clipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ speech-to-clipTests/speech_to_clipTests.swift
@@ -0,0 +1,17 @@
+//
+//  speech_to_clipTests.swift
+//  speech-to-clipTests
+//
+//  Created by Ari Hietamäki on 12.11.2025.
+//
+
+import Testing
+@testable import speech_to_clip
+
+struct speech_to_clipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
